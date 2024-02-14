9849094dd839f76b3183f77f324857ed8060a7ad
Sahara <sahararod@gmail.com>
2024-02-13 18:02:41 -0600

    compara los numeros ingresados hasta que se atine el numero, falta la logica del ordenador

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import random
+#from random import randint
 
 print("¡Bienvenido a ADIVINA EL NÚMERO!")
 print("Por favor escribe cualquier número entre 1 y 100: ")
-guessing = input('--> ')
-print(guessing)
-
+#En python el tipado es dinámico, según el valor asignado es el tipo de dato de la variable
+guessing = int(input('--> '))
+#print(guessing)
 toGuess = random.randint(1,100)
 print(toGuess)
+
+while guessing != toGuess:
+    #convertir a entero, ya que lo guarda como cadena
+    if(guessing > toGuess):
+        print("El número es muy alto")
+    if(guessing < toGuess):
+        print("El número es muy bajo")
+    print("No haz adivinado el número, intenta de nuevo: ")
+    guessing = int(input('--> '))
+print("¡Felicidades haz adivinado el número!")

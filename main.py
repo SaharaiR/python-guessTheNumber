841458e5886ac982fbe7d9aa91e1c4f23628623f
Sahara <sahararod@gmail.com>
2024-02-14 11:07:34 -0600

    lista la logica del ordenador, va un turno y luego otro hasta que se adivine el numero

--- main.py
+++ main.py
@@ -4,17 +4,39 @@ import random
 print("¡Bienvenido a ADIVINA EL NÚMERO!")
 print("Por favor escribe cualquier número entre 1 y 100: ")
 #En python el tipado es dinámico, según el valor asignado es el tipo de dato de la variable
-guessing = int(input('--> '))
+guessingUser = int(input('--> '))
 #print(guessing)
-toGuess = random.randint(1,100)
-print(toGuess)
+toGuess = random.randint(1,10)
+#print(toGuess)
 
-while guessing != toGuess:
-    #convertir a entero, ya que lo guarda como cadena
-    if(guessing > toGuess):
-        print("El número es muy alto")
-    if(guessing < toGuess):
-        print("El número es muy bajo")
+guessingPC = random.randint(1,10)
+#while guessingUser != toGuess:
+while True:
+    print(toGuess)
+    #convertir a entero, ya que el valor inicial lo guarda como cadena
+    if(guessingUser > toGuess):
+        print("Tu número es muy alto")
+    elif(guessingUser < toGuess):
+        print("Tu número es muy bajo")
+    elif(guessingUser == toGuess):
+        print("¡Haz adivinado el número!")
+        break
+    #Aquí tendria que ir la lógica del ordenador
+    print("            ")
+    print("--Mi turno--")
+    print(guessingPC)
+    if(guessingPC > toGuess):
+        print("Mi número fue muy alto")
+    elif(guessingPC < toGuess):
+        print("Mi número fue muy bajo")
+    elif(guessingPC == toGuess):
+        print("¡He adivinado el número!")
+        break
+    guessingPC = random.randint(1,10)
+
+    #Si no adivino el ordenador le toca al usuario
+    print("            ")
+    print("--Tu turno--")
     print("No haz adivinado el número, intenta de nuevo: ")
-    guessing = int(input('--> '))
-print("¡Felicidades haz adivinado el número!")
+    guessingUser = int(input('--> '))
+#print("¡Haz adivinado el numero!")

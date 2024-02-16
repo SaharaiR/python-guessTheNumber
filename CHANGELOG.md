81e82b023f2ba71d4a2c804795c483e7f7408b6f
Sahara <sahararod@gmail.com>
2024-02-15 20:35:37 -0600

    cambie el codigo agregando funciones para cada momento del juego: -generar numero aleatorio -logica del usuario -logica del ordenador, se hizo el primer test de generacion del numero aleatorio

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,6 +12,8 @@
 # Se genero la logica del ordenador, generar otro numero aleatorio, compararlo y disernir si es mayor, menor
 # igual, sino lo es continua el turno para el usuario y así alternando
 # Leer documentación de pruebas unitarias para python (mocks)
+# Cambié el código para usar funciones y facilitar el testing y abarcar mas objetivos de aprendizaje
+# Se logró hacer el primer test del numero aleatorio
 
 ### Added
 
@@ -21,6 +23,8 @@
 # Archivo test_main.py
 
 ### Changed
+
+# Archivo changelog
 
 ### Fixed
 
--- test_main.py
+++ test_main.py
@@ -1,12 +1,24 @@
 import unittest
-from main import adivinaNumero #importamos del modulo la clase que queremos testear
+from main import generateNumber #importamos del modulo la clase que queremos testear
 
 #Por convencion los nombres de las clases en python deben de ir con mayúsculas
 """
-En testing para python tiene clases bases que proporcionan un conjungo de metodos y funciones para 
+-En testing para python tiene clases bases que proporcionan un conjunto de metodos y funciones para 
 facilitar la escritura y ejecución de pruebas unitarias, en este caso se puede usar "TestCase"
-Uso de mocks, al igual que en js, hay que simular para comprobrar lo esperado en el programa
+-Uso de mocks, al igual que en js, hay que simular un valor dado por el usuario para comprobrar lo 
+esperado en el programa
+-Los nombres de los métodos de prueba deben de empezar con "test_" para ser detectados automáticamente
+-"assertTrue" es proporcionado por el objeto "self" (instancia de TestCase) y verifica que la expresión dentro
+de los parentesis sea True
 """
 class TestMain(unittest.TestCase):
+    #Un test sería comprobar que se genera un numero aleatorio entre 1 y 100
+    def test_generateNumber(self):
+        numero = generateNumber()
+        self.assertTrue(1 <= numero <= 100)
     #Un test sería comprobar que el numero proporcionado por el usuario sea mayor, menor o igual
-    pass #palabra reservada para indicar que aquí va código que aún no se hace pero para que no marque error
+    
+    #pass #palabra reservada para indicar que aquí va código que aún no se hace pero para que no marque error
+
+if __name__ == '__main__':
+    unittest.main()

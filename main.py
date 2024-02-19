4b0218178e090544a272d72f6418f15ef5c652d6
Sahara <sahararod@gmail.com>
2024-02-19 12:37:23 -0600

    Se cambiaron las funciones, para uso de funciones puras y hacer más fácil el testing

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import random
 #from random import randint
 #Para hacer testing tenemos que tener funciones o métodos [def]
 """
-Entonces 4 funciones:
+Entonces las funciones:
     1. Función principal
     2. Generar numero aleatorio
-    3. Interactuar con el usuario, pidiendo el numero y comparar
-    4. Logica del ordenador, generando numero aleatorio y comparar
+    3. Comparar el número y resolver si es menor, mayor o igual
     Las tres se van a llamar dentro del bucle en determinado momento
+
 """
 
 def guessNumber():
@@ -15,11 +15,9 @@ def guessNumber():
     print("Por favor escribe cualquier número entre 1 y 100: ")
     #En python el tipado es dinámico, según el valor asignado es el tipo de dato de la variable
     #convertir a entero, ya que el valor inicial lo guarda como cadena
-    global guessingUser 
     guessingUser = int(input('--> '))
     #print(guessing)
     #toGuess = random.randint(1,100)
-    global toGuess 
     toGuess = generateNumber()
     #print(toGuess)
     #guessingPC = random.randint(1,100)
@@ -28,81 +26,37 @@ def guessNumber():
     while True:
         print(toGuess)
         
-        findItUser = userLogic()
+        findItUser = resolveNumber(toGuess, guessingUser)
         if findItUser == True:
+            print("Haz adivinado el número")
             break
-        elif findItUser == False:
-            findItPC = pcLogic()
-            if findItPC == True:
-                break
-            elif findItPC == False:
-                #Si no adivinó el ordenador le toca al usuario
-                print("            ")
-                print("--Tu turno--")
-                print("No haz adivinado el número, intenta de nuevo: ")
-                guessingUser = int(input('--> '))
-        """
-        if(guessingUser > toGuess):
-            print("Tu número es muy alto")
-        elif(guessingUser < toGuess):
-            print("Tu número es muy bajo")
-        elif(guessingUser == toGuess):
-            print("¡Haz adivinado el número!")
-            break
-        """
-        #Aquí tendria que ir la lógica del ordenador
-        """
-        print("            ")
-        print("--Mi turno--")
-        global guessingPc
-        guessingPC = genearteNumber()
+        guessingPC = generateNumber()
+        print(" ")
+        print("--Mi turno:--")
         print(guessingPC)
-        if(guessingPC > toGuess):
-            print("Mi número fue muy alto")
-        elif(guessingPC < toGuess):
-            print("Mi número fue muy bajo")
-        elif(guessingPC == toGuess):
-            print("¡He adivinado el número!")
+        findItPC = resolveNumber(toGuess, guessingPC)
+        if findItPC == True:
+            print("He adivinado el número")
             break
-        #Se genera un nuevo nuevo aleatorio para el turno del ordenador
-        guessingPC = random.randint(1,100)
-        
         #Si no adivinó el ordenador le toca al usuario
-        print("            ")
+        print(" ")
         print("--Tu turno--")
         print("No haz adivinado el número, intenta de nuevo: ")
         guessingUser = int(input('--> '))
-        """
-
+                
 def generateNumber():
     return random.randint(1,100)
 
-def userLogic():
-    if(guessingUser > toGuess):
-        print("Tu número es muy alto")
-        return False
-    elif(guessingUser < toGuess):
-        print("Tu número es muy bajo")
-        return False
-    elif(guessingUser == toGuess):
-        print("¡Haz adivinado el número!")
+def resolveNumber(toGuessNumber, guessingNumber):
+    if(guessingNumber > toGuessNumber):
+        print("El número fue muy alto")
+        #return "high"
+    elif(guessingNumber < toGuessNumber):
+        print("El número fue muy bajo")
+        #return "low"
+    elif(guessingNumber == toGuessNumber):
         return True
-
-def pcLogic():
-    print("            ")
-    print("--Mi turno--")
-    guessingPC = generateNumber()
-    print(guessingPC)
-    if(guessingPC > toGuess):
-        print("Mi número fue muy alto")
-        return False
-    elif(guessingPC < toGuess):
-        print("Mi número fue muy bajo")
-        return False
-    elif(guessingPC == toGuess):
-        print("¡He adivinado el número!")
-        return True
-
+        
 if __name__ == "__main__":
     guessNumber()
 

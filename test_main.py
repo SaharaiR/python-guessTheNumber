f5a87cc9e7e974800f4ef66fbc7325a73ee26219
Sahara <sahararod@gmail.com>
2024-02-20 10:41:15 -0600

    prueba unitaria a la funcion resolveNumber

--- test_main.py
+++ test_main.py
@@ -1,5 +1,7 @@
 import unittest
-from main import generateNumber #importamos del modulo la clase que queremos testear
+from main import generateNumber,resolveNumber #importamos del modulo la clase que queremos testear
+from unittest.mock import patch
+from io import StringIO
 
 #Por convencion los nombres de las clases en python deben de ir con mayúsculas
 """
@@ -14,9 +16,41 @@ de los parentesis sea True
 class TestMain(unittest.TestCase):
     #Un test sería comprobar que se genera un numero aleatorio entre 1 y 100
     def test_generateNumber(self):
-        numero = generateNumber()
-        self.assertTrue(1 <= numero <= 100)
+        number = generateNumber()
+        self.assertTrue(1 <= number <= 100)
+
+    """
+    def setUp(self):
+        # Configuramos el parche en el método 'setUp' para que se aplique a cada método de prueba
+        self.mock_stdout_patch = patch('sys.stdout', new_callable=StringIO)
+        self.mock_stdout = self.mock_stdout_patch.start()
+
+    def tearDown(self):
+        # Detenemos el parche en el método 'tearDown' para limpiar después de cada prueba
+        self.mock_stdout_patch.stop()
+    """
     #Un test sería comprobar que el numero proporcionado por el usuario sea mayor, menor o igual
+    """
+    patch indica que el siguiente objeto o función será reemplazado temporalmente por algo 
+    diferente durante la ejecución de la prueba.
+    en este caso se sustituira lo que presenta en consola (sys.stdout)
+    y "new_callable=StringIO" indica que se utilizar un nuevo objeto StringIO y al utilizar como
+    new_callable indica que la salida estándar será sustituida por un objeto StringIO
+    """
+    @patch('sys.stdout', new_callable=StringIO)
+    #self es una convención para utilizar como primer parametro. Se refiere a la instancia de la clase,
+    #proporciona acceso a los métodos y atributos de la clase de prueba y el otro parametro para recibir
+    #para recibir el objeto de parche (el mock) que es la salida stándar
+    def test_resolveNumber(self,mock_stdout):
+        numberToGuess = 10
+        numberToResolve = 20 
+        printExpected = resolveNumber(numberToGuess, numberToResolve)
+        #capturamos la salida en pantalla, mock_stdout es el objeto, obtenemos el valor (getvalue) y con strip
+        #se eliminan los espacios en blanco al inicio o final
+        output = mock_stdout.getvalue().strip()
+        #comparar la salida con la esperada
+        self.assertEqual(output, "El número fue muy alto")
+        self.assertIsNone(printExpected)
     
     #pass #palabra reservada para indicar que aquí va código que aún no se hace pero para que no marque error
 
